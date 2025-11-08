109c8d89d9ece72c66f7cda3587b481a65ca55db
pixrayconcontaforte-pixel <pixrayconcontaforte@gmail.com>
2025-11-08 14:48:58 -0300

    Delete emit.php

--- emit.php
+++
@@ -1,5 +0,0 @@
-<?php
-$host="localhost"; $db="site_carteira"; $user="root"; $pass="";
-try{ $pdo=new PDO("mysql:host=$host;dbname=$db;charset=utf8",$user,$pass); $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);}
-catch(PDOException $e){ die(json_encode(['success'=>false,'msg'=>"Erro na conexão: ".$e->getMessage()])); }
-?>
